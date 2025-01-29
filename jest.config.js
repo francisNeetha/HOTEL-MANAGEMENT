a5d18c2e5db12db1c4d7c6bbd20d0849021181a1
neethafrancis26 <neetha.f@coddletech.com>
2025-01-29 14:09:18 +0530

    updated signup component

--- jest.config.js
+++ jest.config.js
@@ -6,11 +6,14 @@ module.exports = {
   collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  "setupFilesAfterEnv": ["<rootDir>/src/setupTests.ts"],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
   },
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  
 };
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -5,3 +5,7 @@
 // src/setupTests.ts
 import '@testing-library/jest-dom';
 
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder as any;
